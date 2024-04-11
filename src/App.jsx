887e1dec51c19f453c53057246abc06935def0f7
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-04-11 22:42:01 +0200

    logic for adding items to shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Product from './components/product';
 import ShoppingCart from './components/shopping-card';
 
 
-function App() {
-  return (
-    <React.Fragment>
+class App extends Component {
+  state = {
+    items: []
+  }
+
+  addItem = (amount, name, price) => {
+    let currentItems = this.state.items;
+
+    let existingItem = this.state.items.find(item => item.name === name)
+    if(existingItem) {
+      existingItem.amount += amount;
+      existingItem.totalPrice += price;
+    } else {
+    currentItems.push({
+      amount,
+      name,
+      price,
+      totalPrice: price
+    });
+  }
+    this.setState({ items: currentItems})
+    console.log(this.state)
+  }
+
+  render() {
+    return <React.Fragment>
       <Navbar />
       <div className='main-container'>
         <div className='product-container'>
-          <Product image="bowl.jpg" title="Healthy Bowl" description="Füge Healthy Bowl zu deinem Warenkorb hinzu" />
-          <Product image="avocado_toast.jpg" title="Avocado Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu" />
-          <Product image="frensh_toast.jpg" title="Frensh Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu" />
-          <Product image="pancakes.jpg" title="Pancakes" description="Füge Pancakes zu deinem Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Healthy Bowl', 7.99)} image="bowl.jpg" title="Healthy Bowl" description="Füge Healthy Bowl zu deinem Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Avocado Toast', 6.99)} image="avocado_toast.jpg" title="Avocado Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Frensh Toast', 4.99)} image="frensh_toast.jpg" title="Frensh Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Pancakes', 5.99)} image="pancakes.jpg" title="Pancakes" description="Füge Pancakes zu deinem Warenkorb hinzu" />
         </div>
-        <ShoppingCart />
+        <ShoppingCart items={this.state.items}/>
       </div>
     </React.Fragment>
-  );
+  };
 }
 
 export default App;
--- src/components/shopping-card.jsx
+++ src/components/shopping-card.jsx
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
-    state = {
-        items: [
-            {
-                amount: 1,
-                name: 'Healthy Bowl',
-                price: 7.9
-            }, 
-            {
-                amount: 1,
-                name: 'Avocado Toast',
-                price: 8.9
-            },
-        ]
-    }
+    state = {}
 
 
     render() {
         return <div className='shopping-card'>
             <h2>Warenkorb</h2>
-            {this.state.items.map(item => <div key={item.name}>{item.amount}x {item.name} {item.price}€</div>)}
+            {this.props.items.map(item => <div key={item.name}>{item.amount}x {item.name} {item.totalPrice}€</div>)}
         </div>;
     }
 }
