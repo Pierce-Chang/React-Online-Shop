84310fcf6584f92d1f2985bd1d8190c909a5a6ec
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-04-11 22:03:02 +0200

    added shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Product from './components/product';
+import ShoppingCart from './components/shopping-card';
 
 
 function App() {
   return (
-    <React.Fragment className="App">
+    <React.Fragment>
       <Navbar />
-      <div className='product-container'>
-        <Product image="bowl.jpg" title="Healthy Bowl" description="Füge Healthy Bowl zu deinem Warenkorb hinzu"/>
-        <Product image="avocado_toast.jpg" title="Avocado Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu"/>
-        <Product image="frensh_toast.jpg" title="Frensh Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu"/>
-        <Product image="pancakes.jpg" title="Pancakes" description="Füge Pancakes zu deinem Warenkorb hinzu"/>
+      <div className='main-container'>
+        <div className='product-container'>
+          <Product image="bowl.jpg" title="Healthy Bowl" description="Füge Healthy Bowl zu deinem Warenkorb hinzu" />
+          <Product image="avocado_toast.jpg" title="Avocado Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu" />
+          <Product image="frensh_toast.jpg" title="Frensh Toast" description="Füge Avocado Toast zu deinem Warenkorb hinzu" />
+          <Product image="pancakes.jpg" title="Pancakes" description="Füge Pancakes zu deinem Warenkorb hinzu" />
+        </div>
+        <ShoppingCart />
       </div>
     </React.Fragment>
   );
